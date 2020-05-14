016ce9f44368f84c95bffa5afb5d54d24eb10700
Houchengisnull <609937364@qq.com>
2020-05-14 20:02:45 +0800

    20200508 线程工具复习Fork-Join\CountDownLatch\CyclicBarrier\Semaphore; Use Exchanger demo代码

---
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/exchage/UseExchage.java
@@ -0,0 +1,38 @@
+package org.hc.learning.thread.exchage;
+
+import java.util.concurrent.Exchanger;
+
+public class UseExchage {
+
+    static Exchanger<String> exchanger = new Exchanger<>();
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message = "AAA";
+                try {
+                    message = exchanger.exchange(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " receive message:"+ message);
+            }
+        }, "A").start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message = "BBB";
+                try {
+                    message = exchanger.exchange(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " receive message:"+ message);
+            }
+        }, "B").start();
+
+    }
+}
