61b3b9b8d67b0b75e5b97a6896dd25a29c62934b
Houchengisnull <609937364@qq.com>
2020-06-03 20:54:17 +0800

    20200603 Future 线程回调

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/future/UseCallable.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/future/UseCallable.java
@@ -1,4 +1,29 @@
 package org.hc.learning.thread.future;
 
-public class UseCallable {
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class UseCallable implements Callable<Integer> {
+
+    private Integer var = 1024;
+
+    @Override
+    public Integer call() throws Exception {
+        Thread.interrupted();
+        System.out.println("Thread.interrupted:" + Thread.interrupted());
+        return var;
+    }
+
+    public static void main(String[] args) throws Exception {
+        UseCallable useCallable = new UseCallable();
+        FutureTask<Integer> integerFutureTask = new FutureTask<>(useCallable);
+        Thread thread = new Thread(integerFutureTask);
+        thread.start();
+
+        // System.out.println(useCallable.call());
+        System.out.println(integerFutureTask.get());
+        System.out.println(integerFutureTask.isDone());
+        System.out.println(integerFutureTask.isCancelled());
+        System.out.println(thread.isInterrupted());
+    }
 }
--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/future/UseFuture.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/future/UseFuture.java
@@ -1,5 +1,25 @@
 package org.hc.learning.thread.future;
 
-public class UseFuture {
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
+public class UseFuture implements Callable {
+
+    private Integer ran = 0;
+
+    @Override
+    public Integer call() throws Exception {
+        while (ran++ < 100) {
+            System.out.println("useFuture : " + ran);
+            Thread.sleep(100);
+        }
+        return ran;
+    }
+
+    public static void main(String[] args) throws Exception {
+        UseFuture useFuture = new UseFuture();
+        FutureTask futureTask = new FutureTask<>(useFuture);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+    }
 }
