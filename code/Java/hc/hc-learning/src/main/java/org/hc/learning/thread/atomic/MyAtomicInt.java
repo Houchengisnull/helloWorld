3a724e0426b5a73ed4a6cc628743c481bc865188
Houchengisnull <609937364@qq.com>
2020-06-14 23:20:57 +0800

    20200604 CAS作业与thread.join()使线程有序

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/atomic/MyAtomicInt.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/atomic/MyAtomicInt.java
@@ -1,41 +1,55 @@
+package org.hc.learning.thread.atomic;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class HalfAtomicInt {
-    private AtomicInteger atomicI = new AtomicInteger(0);
+public class MyAtomicInt {
 
-    /*请完成这个递增方法*/
-    public void increament() {
-        for (;;) {
-            int i = getCount();
-            boolean suc = compareAndSet(i, ++i);
+    private AtomicInteger value = new AtomicInteger(0);
+
+    /**
+     * 自增 atomicI++
+     */
+    public void increment() {
+        for(;;) {
+            int count = getCount();
+            boolean suc = compareAndSet(count, ++count);
             if (suc) {
                 break;
             }
         }
+        /*value.incrementAndGet();*/
     }
 
     public int getCount() {
-        return atomicI.get();
+        return value.get();
     }
 
-    public boolean compareAndSet(int oldValue,int newValue){
-        return atomicI.compareAndSet(oldValue,newValue);
+    public boolean compareAndSet(Integer oldValue, Integer newValue) {
+        return value.compareAndSet(oldValue,newValue);
+
     }
 
     public static void main(String[] args) throws InterruptedException {
-        HalfAtomicInt halfAtomicInt = new HalfAtomicInt();
+        MyAtomicInt atomicInt = new MyAtomicInt();
         for(int i=0;i<10;i++){
-            new Thread(new Runnable() {
+            Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     for(int i=0;i<1000;i++){
-                        halfAtomicInt.increament();
+                        atomicInt.increment();
                     }
                 }
-            }).start();
+            });
+            thread.start();
+            /**
+             * 不应该用 thread.join()方法
+             * 因为join方法是通过synchronized实现的 将以当前线程作为锁资源
+             * 注意 thread.join()当前线程为 Main线程
+             * 故导致所有线程同步执行
+             */
+            /*thread.join();*/
         }
-        Thread.sleep(1000);
-        System.out.println(halfAtomicInt.getCount());
+        Thread.sleep(10);
+        System.out.println(atomicInt.getCount());
     }
-
 }
--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/base/UseJoin.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/base/UseJoin.java
@@ -1,4 +1,22 @@
 package org.hc.learning.thread.base;
 
 public class UseJoin {
+    /**
+     * Join将使线程执行有序
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                }
+            }, i + "");
+            thread.start();
+            thread.join();
+        }
+    }
+
 }
