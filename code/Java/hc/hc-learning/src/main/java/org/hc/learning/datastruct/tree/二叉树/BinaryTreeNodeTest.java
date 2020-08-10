137bb3a024e0aa6149d295cbbefe0f5e22aa034c
Houchengisnull <609937364@qq.com>
2020-08-10 16:30:12 +0800

    20200810 remove unused import

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/datastruct/tree/二叉树/BinaryTreeNodeTest.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/datastruct/tree/二叉树/BinaryTreeNodeTest.java
@@ -2,8 +2,6 @@ package org.hc.learning.datastruct.tree.二叉树;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
