ec10621a25b54233e13f613d8791d32d27537e55
Houchengisnull <609937364@qq.com>
2020-11-10 10:02:57 +0800

    20201110 swing

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/swing/PainterApplication.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/swing/PainterApplication.java
@@ -1,4 +1,36 @@
 package org.hc.learning.swing;
 
-public class PainterApplication {
+import javax.swing.*;
+import java.awt.*;
+
+public class PainterApplication extends JFrame{
+
+    public static void main(String[]argv)
+    {
+        JFrame frame=new PainterApplication();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭操作
+        frame.setSize(600, 600);
+        frame.setVisible(true);//显示 设置大小
+    }
+    public void paint(Graphics g)//画图对象
+    {
+        g.drawString("Circle 99", 20, 20);//名称 横位置 纵位置
+        int x0=getSize().width/2;
+        int y0=getSize().height/2;//圆心
+        for(int r=0;r<getSize().height/2;r+=10)
+        {
+            g.setColor(getRandomColor());
+            g.drawOval(x0- r,y0- r,r*2,r*2);//椭圆
+        }
+    }
+
+    Color getRandomColor()//随机数
+    {
+        return new Color(
+                (int)(Math.random()*255),
+                (int)(Math.random()*255),
+                (int)(Math.random()*255)
+        );
+    }
+
 }
--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/swing/RecPainter.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/swing/RecPainter.java
@@ -1,4 +1,57 @@
 package org.hc.learning.swing;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+
 public class RecPainter {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(new TestPane());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
+
+class TestPane extends JPanel {
+    TriangleShape triangleShape;
+    Polygon poly;
+    public TestPane() {
+        triangleShape = new TriangleShape(
+                new Point2D.Double(50, 0),
+                new Point2D.Double(100, 100),
+                new Point2D.Double(0, 100));
+        poly = new Polygon(new int[] { 50, 100, 0 }, new int[] { 0, 100, 100 }, 3);
+    }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(200, 200);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setColor(Color.RED);
+        g2d.fill(poly);
+
+        g2d.setColor(Color.GREEN);
+        g2d.translate(0, 100);
+        g2d.fill(triangleShape);
+        g2d.dispose();
+    }
+}
+class TriangleShape extends Path2D.Double {
+
+    public TriangleShape(Point2D... points) {
+        moveTo(points[0].getX(), points[0].getY());
+        lineTo(points[1].getX(), points[1].getY());
+        lineTo(points[2].getX(), points[2].getY());
+        closePath();
+    }
 }
