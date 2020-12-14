703f3b986b07864ae299a9eddb921fa3cbf1faa7
Houchengisnull <609937364@qq.com>
2020-12-14 16:20:39 +0800

    20201214 自己实现线程池

---
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/pool/MyThreadPool.java
@@ -0,0 +1,69 @@
+package org.hc.learning.thread.pool;
+
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class MyThreadPool implements Executor {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyThreadPool.class);
+
+    public static final int DEFAULT_TASK_NUM = 16;
+    public static final int DEFAULT_THREAD_NUM = 8;
+    /**
+     * 保存任务
+     */
+    private BlockingQueue<Runnable> taskQueue;
+    /**
+     * 保存线程
+     */
+    private MyThread[] threads;
+
+    public MyThreadPool() {
+        this(DEFAULT_TASK_NUM, DEFAULT_THREAD_NUM);
+    }
+
+    public MyThreadPool(int taskNum, int threadNum){
+        this.taskQueue = new LinkedBlockingDeque<>(taskNum);
+        this.threads = new MyThread[threadNum];
+        // 初始化所有线程并且启动
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new MyThread();
+            threads[i].start();
+            logger.debug("my pool {} start", threads[i].getName());
+        }
+    }
+
+    /**
+     * 该方法仅用于将任务保存到任务队列，待线程处理
+     * @param command
+     */
+    @SneakyThrows
+    @Override
+    public void execute(Runnable command) {
+        taskQueue.put(command);
+    }
+
+    /**
+     * 为控制线程的生命周期
+     * 我们需要重写Thread
+     */
+    class MyThread extends Thread {
+        /**
+         * 阻塞地获取任务
+         */
+        @SneakyThrows
+        @Override
+        public void run() {
+            while (true) {
+                Runnable task = taskQueue.take();
+                logger.debug("my pool {} run", Thread.currentThread().getName());
+                task.run();
+            }
+        }
+    }
+}
--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/pool/TestMyThreadPool.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/pool/TestMyThreadPool.java
@@ -1,13 +1,39 @@
 package org.hc.learning.thread.pool;
 
-public class TestMyThreadPool {
-	// 任务数量大于线程数量
-	public static void main(String[] args) {
-		
+import lombok.SneakyThrows;
+import org.hc.learning.test.TestCase;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMyThreadPool extends TestCase {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestMyThreadPool.class);
+	private static final int taskNum = 32;
+	private MyThreadPool pool = new MyThreadPool();
+
+	@Test
+	public void 提交任务() throws InterruptedException {
+		for (int i = 0; i < taskNum; i++) {
+			pool.execute(new PoolTestTask(i));
+		}
+		Thread.sleep(1000);
 	}
-	
-	// TODO
-	/*public  class TestMyThreadPool implements Runnable {
-		
-	}*/
+
+	static class PoolTestTask implements Runnable {
+
+		private int taskSeq;
+
+		PoolTestTask(int taskSeq) {
+			this.taskSeq = taskSeq;
+		}
+
+		@SneakyThrows
+		@Override
+		public void run() {
+			Thread.sleep(50);
+			logger.debug("Task {} has been finished", taskSeq);
+		}
+	}
+
 }
