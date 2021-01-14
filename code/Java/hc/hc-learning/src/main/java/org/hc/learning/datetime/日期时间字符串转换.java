4948057288d93c3643789eee4a77137b1211b205
Houchengisnull <609937364@qq.com>
2021-01-14 13:59:42 +0800

    20200114 JDK8 DateTime、Instant、String相互转化

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/datetime/日期时间字符串转换.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/datetime/日期时间字符串转换.java
@@ -1,4 +1,72 @@
 package org.hc.learning.datetime;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
 public class 日期时间字符串转换 {
+
+    DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Test
+    public void localDateTime2String(){
+        LocalDateTime now = LocalDateTime.now();
+        String format = now.format(pattern);
+        log.debug("dateTime >> string -------- dateTime.format() >> {}", format);
+    }
+
+    /**
+     * DateTime >> 时间戳
+     * 时间戳   >> long
+     */
+    @Test
+    public void localDateTime2Instant(){
+        LocalDateTime now = LocalDateTime.now();
+        Instant instant = now.atZone(ZoneId.systemDefault()).toInstant();
+        log.debug("dateTime >> instant -------- dateTime.toInstant() >> {}", instant);
+    }
+
+    @Test
+    public void instant2String() {
+        Instant now = Instant.now();
+        String nowString = now.toString();
+        log.debug("instant >> string -------- instant.toString() >> {}", nowString);
+    }
+
+    @Test
+    public void instant2Long() {
+        Instant now = Instant.now();
+        long l = now.toEpochMilli();
+        log.debug("instant >> long -------- instant.toEpochMilli() >> {}", l);
+    }
+
+    @Test
+    public void instant2DateTime() {
+        Instant now = Instant.now();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(now, ZoneId.systemDefault());
+        log.debug("instant >> dateTime -------- LocaleDateTime.ofInstant() >> {}", localDateTime);
+    }
+
+    @Test
+    public void string2DateTime() {
+        String string = "2021-01-14 03:26:00";
+        LocalDateTime datetime = LocalDateTime.parse(string, pattern);
+        log.debug("string >> LocalDate -------- LocalDateTime.parse(String, DateTimeFormatter) >> {}", datetime);
+        log.debug("string >> LocalDate -------- LocalDateTime.parse(String, DateTimeFormatter) [北京时间戳] >> {}", datetime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+    }
+
+    @Test
+    public void string2Instant() {
+        String string = "2021-01-14 03:26:00";
+        Instant instant = LocalDateTime.parse(string, pattern).toInstant(ZoneOffset.UTC);
+        log.debug("string >> Instant -------- LocalDateTime.parse().toInstant() >> {}", instant);
+        log.debug("string >> Instant -------- LocalDateTime.parse().toInstant().toEpochMilli() [UTC时间戳]>> {}", instant.toEpochMilli());
+    }
+
 }
