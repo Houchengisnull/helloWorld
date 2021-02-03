e46cb631e8f3f2540dd976c8fc8c0c02207891ca
Houchengisnull <609937364@qq.com>
2021-02-03 13:58:50 +0800

    20210203 修改文件编码

--- code/Java/hc11/src/main/java/com/houc/module/ModuleApplication.java
+++ code/Java/hc11/src/main/java/com/houc/module/ModuleApplication.java
@@ -3,14 +3,13 @@ package com.houc.module;
 /**
  * 编译命令
  * 1.`javac -d target/hc src/main/java/module-info.java src/main/java/com/houc/module/ModuleApplication.java`
- * 2.`javac -mod mods
  *
  * 运行命令
- * 1.`java --module-path target -m hc/com.houc.module.ModuleApplication
- *      结果为"hc"
+ * 1.`java --module-path target -m hc/com.houc.module.ModuleApplication`
+ *      运行结果: hc
  * 直接运行
  * 1.`java -classpath target/hc com.houc.module.ModuleApplication`
- *      结果为空
+ *      运行结果: null
  */
 public class ModuleApplication {
 
--- code/Java/hc11/src/main/java/module-info.java
+++ code/Java/hc11/src/main/java/module-info.java
@@ -1,2 +1,2 @@
-module $MODULE_NAME$ {
+module hc {
 }
