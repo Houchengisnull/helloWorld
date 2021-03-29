5b3fbbd14b04167267c86a3153125699c65aec79
Houchengisnull <609937364@qq.com>
2021-03-29 19:57:28 +0800

    2021-3-29 CompletionService梳理

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/pool/CompletionServiceCase.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/thread/pool/CompletionServiceCase.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,26 +14,40 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CompletionServiceCase extends TestCase {
 
     private final int POOL_SIZE = Runtime.getRuntime().availableProcessors();
-    private final int TOTAL_TASK = Runtime.getRuntime().availableProcessors() * 10;
-    private final long sleepTime = 100L;
+    private final int TOTAL_TASK = Runtime.getRuntime().availableProcessors() * 5;
+    private final int TIMES = 10;
+    private final int SLEEP_RANGE = 10;
+    /*private final long sleepTime = 100L;*/
     private static AtomicInteger count = new AtomicInteger(0);
+
+    class CallTask implements Callable<Integer> {
+
+        Integer taskId;
+
+        CallTask (Integer taskId) {
+            this.taskId = taskId;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            long sleepTime = new Random().nextInt(SLEEP_RANGE) * TIMES;
+            /*log.debug("任务[{}]休眠{}ms", taskId, sleepTime);*/
+            Thread.sleep(sleepTime);
+            return taskId;
+        }
+    }
 
     @Test
     public void 对比() throws ExecutionException, InterruptedException {
         ExecutorService pool = Executors.newFixedThreadPool(POOL_SIZE);
         List<Future<Integer>> tasks = new LinkedList<>();
         for (int i = 0; i < TOTAL_TASK; i++) {
-            Future<Integer> submit = pool.submit(new Callable<Integer>() {
-                @Override
-                public Integer call() throws Exception {
-                    Thread.sleep(sleepTime);
-                    return count.addAndGet(1);
-                }
-            });
+            Future<Integer> submit = pool.submit(new CallTask(i));
             tasks.add(submit);
         }
 
         for (int i = 0; i < tasks.size(); i++) {
+            // FutureTask.get()是一个阻塞方法
             Integer integer = tasks.get(i).get();
             log.debug("Task {} execute end", integer);
         }
@@ -50,18 +65,13 @@ public class CompletionServiceCase extends TestCase {
         ExecutorCompletionService<Integer> service = new ExecutorCompletionService<>(pool);
 
         for (int i = 0; i < TOTAL_TASK; i++) {
-            service.submit(new Callable<Integer>() {
-                @Override
-                public Integer call() throws Exception {
-                    Thread.sleep(sleepTime);
-                    return count.addAndGet(1);
-                }
-            });
+            Future<Integer> submit = service.submit(new CallTask(i));
         }
 
         for (int i = 0; i < TOTAL_TASK; i++) {
-            Integer integer = service.take().get();
-            log.debug("Task {} execute end", integer);
+            // 调用take()获得先完成的任务
+            Integer taskId = service.take().get();
+            log.debug("Task {} execute end", taskId);
         }
     }
 
