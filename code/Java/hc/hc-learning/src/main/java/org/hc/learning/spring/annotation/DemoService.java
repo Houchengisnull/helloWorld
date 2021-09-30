c226aee8baf04d16f2e01905035ae3dceeeedaa8
Houchengisnull <609937364@qq.com>
2021-09-30 16:10:17 +0800

    # spring - @Service("Demo") 注解声明实例名

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/spring/annotation/DemoService.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/spring/annotation/DemoService.java
@@ -1,10 +1,19 @@
 package org.hc.learning.spring.annotation;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 
-@Service
-public class DemoService {
+@Service("Demo")
+public class DemoService implements BeanNameAware {
+
+	private String beanName;
+
 	public void outputResult() {
-		System.out.println("从组合注解中获得Bean");
+		System.out.println(beanName + "从组合注解中获得Bean");
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		this.beanName = name;
 	}
 }
