7ad197d0cf68b51302d706aab344626fa9f3071b
Houchengisnull <609937364@qq.com>
2022-04-29 19:01:56 +0800

    1. 先执行connect, 再执行获取流 2. 释放

--- code/Java/hc/hc-learning/src/main/java/org/hc/learning/net/bio/Client.java
+++ code/Java/hc/hc-learning/src/main/java/org/hc/learning/net/bio/Client.java
@@ -1,5 +1,7 @@
 package org.hc.learning.net.bio;
 
+import lombok.SneakyThrows;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -10,17 +12,43 @@ public class Client {
 
     public static void main(String[] args) {
         InetSocketAddress addr = new InetSocketAddress("127.0.0.1", 10001);
-        try (
-            Socket socket = new Socket();
-            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream input = new ObjectInputStream(socket.getInputStream())
-        ) {
+        Socket socket = null;
+        ObjectOutputStream output = null;
+        ObjectInputStream input = null;
+        try {
+            socket = new Socket();
             socket.connect(addr);
+            // 必须执行connect后, 才有stream
+            output = new ObjectOutputStream(socket.getOutputStream());
+            input = new ObjectInputStream(socket.getInputStream());
+
             output.writeUTF("Mark");
             output.flush();
             System.out.println(input.readUTF());
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
