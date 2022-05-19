b779cf5dc6dd469c4d22f798fd3ed95fa103300f
Houchengisnull <609937364@qq.com>
2022-05-19 20:45:25 +0800

    1.spring boot <version>1.5.8</version> >> <version>2.5.3</version> 2.{*spring} @PathVariable

--- code/Java/hc/hc-web/src/main/java/hc/web/controller/TestController.java
+++ code/Java/hc/hc-web/src/main/java/hc/web/controller/TestController.java
@@ -3,6 +3,7 @@ package hc.web.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,5 +28,25 @@ public class TestController {
         ResponseEntity<String> res = restTemplate.getForEntity("http://127.0.0.1:8090/hc-web/test/", String.class);
         return res.getBody();
     }
+
+    /**
+     * {*spring}
+     */
+    @GetMapping("/{*path}")
+    public String pathTest(@PathVariable("path") String path) {
+        return path;
+    }
+
+    /**
+     * 错误示例:
+     * /{method}//{*path} 若使用{*path},则仅支持一个@PathVariables
+     * @param uri
+     * @param path
+     * @return
+     */
+    /*@GetMapping("/{method}//{*path}")
+    public String pathTest(@PathVariable("method")String uri, @PathVariable("path") String path) {
+        return path;
+    }*/
 
 }
