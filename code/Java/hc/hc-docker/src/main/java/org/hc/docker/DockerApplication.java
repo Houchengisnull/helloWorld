c5a5967ed1393b5805af3bfe5c7a56c96c663c7f
Houchengisnull <609937364@qq.com>
2023-12-28 21:33:26 +0800

    1. docker

---
+++ code/Java/hc/hc-docker/src/main/java/org/hc/docker/DockerApplication.java
@@ -0,0 +1,16 @@
+package org.hc.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用于学习Docker
+ */
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class);
+    }
+
+}
