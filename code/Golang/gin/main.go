22615a3d18fbf12484fb21d8f789cb70e76f36b2
Houchengisnull <609937364@qq.com>
2024-03-15 16:20:55 +0800

    1. gin

---
+++ code/Golang/gin/main.go
@@ -0,0 +1,57 @@
+﻿package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetRuntimePath() string {
+	str, _ := os.Getwd()
+	return str
+}
+
+func main() {
+	runtimePath := GetRuntimePath()
+	fmt.Println("当前路径:" + runtimePath)
+
+	// router
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// AsciiJSON
+	router.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "<br>",
+		}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+
+	router.LoadHTMLGlob("./templates/*/**")
+
+	/* router.GET("/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main Website",
+		})
+	}) */
+
+	router.GET("/posts/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
+			"title": "Posts",
+		})
+	})
+	router.GET("/users/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
+			"title": "Users",
+		})
+	})
+
+	router.Run(":8080")
+}
