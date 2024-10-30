3599c09ea074a2495cee70f92cda3c636a3e39e4
houcheng <609937364@qq.com>
2024-10-30 11:00:05 +0800

    1.HelloElectron

---
+++ code/NodeJs/Electron/HelloElectron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// 关闭所有窗口时退出APP
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
